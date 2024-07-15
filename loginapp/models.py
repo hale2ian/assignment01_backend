b21838afb930f95dd6554e07253abe05f4ebe084
20230922 <jepa309@ess.ais.ac.nz>
2024-07-15 21:34:17 +1200

    configured rest framework, database

---
+++ loginapp/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+
+class ChatRoom(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+class Message(models.Model):
+    room = models.ForeignKey(ChatRoom, on_delete=models.CASCADE)
+    sender = models.CharField(max_length=100)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.message
