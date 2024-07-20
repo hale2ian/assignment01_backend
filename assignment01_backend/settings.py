be425ce622b9a042c18ac40080b23ac5613e21f5
20230922 <jepa309@ess.ais.ac.nz>
2024-07-20 17:33:23 +1200

    settings.py and verceldb

--- assignment01_backend/settings.py
+++ assignment01_backend/settings.py
@@ -57,8 +57,7 @@ ROOT_URLCONF = 'assignment01_backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -130,7 +129,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
+CORS_ALLOW_ALL_ORIGINS = True  # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
 
 CORS_ALLOW_CREDENTIALS = True
 
@@ -138,7 +137,7 @@ CORS_ALLOWED_ORIGINS = [
 
     'http://localhost:3000',
 
-] # If this is used, then not need to use `CORS_ALLOW_ALL_ORIGINS = True`
+]  # If this is used, then not need to use `CORS_ALLOW_ALL_ORIGINS = True`
 
 CORS_ALLOWED_ORIGIN_REGEXES = [
 
