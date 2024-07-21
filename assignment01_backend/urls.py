4c4846eeb3641ee180a16e3b80b243512f3c16cc
20230922 <jepa309@ess.ais.ac.nz>
2024-07-21 14:25:13 +1200

    forgot to edit urls.py for registration feature

--- assignment01_backend/urls.py
+++ assignment01_backend/urls.py
@@ -17,10 +17,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
+from loginapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('loginapp/', include('loginapp.urls')),
     path('auth/', obtain_auth_token),
+    path('auth/register/', views.RegisterView.as_view(), name='register'),
 ]
